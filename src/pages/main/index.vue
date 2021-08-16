3c2560ecdae9507fd6a93491bd33b56eeb3732a4
Zoom <1113525779@qq.com>
2021-08-16 22:09:00 +0800

    todo任务菜单开发

--- src/pages/main/index.vue
+++ src/pages/main/index.vue
@@ -5,22 +5,49 @@
         <img :src="userInfo.avatar" alt="用户头像" />
         <span class="user-name">{{ userInfo.name }}</span>
       </header>
-      <div class="main-menu">
-        <div v-for="item in staticMenu" :key="item.type" class="main-menu-item">
+      <div class="menu main-menu">
+        <div
+          v-for="item in staticMenu"
+          :key="item.value"
+          class="menu-item"
+          :class="{ 'is-active': activeMenu === item.value }"
+          @click="toActive(item)"
+        >
+          <div class="active-line"></div>
           <i :class="item.icon"></i>
           <span>{{ item.name }}</span>
-          <span class="count" v-if="menuNumMap[item.type]">
-            {{ menuNumMap[item.type] }}
+          <span class="count" v-if="menuNumMap[item.value]">
+            {{ menuNumMap[item.value] }}
           </span>
         </div>
       </div>
       <el-divider></el-divider>
+      <div class="menu task-menu">
+        <div
+          v-for="item in taskMenuList"
+          :key="item.value"
+          class="menu-item"
+          :class="{ 'is-active': activeMenu === item.value }"
+          @click="toActive(item)"
+        >
+          <div class="active-line"></div>
+          <i class="el-icon-date"></i>
+          <span>{{ item.name }}</span>
+          <span class="count" v-if="item.tasks.length">
+            {{ item.tasks.length }}
+          </span>
+        </div>
+      </div>
+      <div class="create-area">
+        <i class="el-icon-plus"></i>
+        <span>新建列表</span>
+      </div>
     </aside>
     <div class="right-content"></div>
   </div>
 </template>
 <script lang="ts">
-import { reactive, ref, toRefs } from '@vue/reactivity'
+import { reactive, ref } from '@vue/reactivity'
 import avatar from '../../assets/images/logo.png'
 
 export default {
@@ -30,8 +57,8 @@ export default {
       avatar,
     })
     const staticMenu = [
-      { type: 'star', name: '重要', icon: 'el-icon-star-off' },
-      { type: 'task', name: '任务', icon: 'el-icon-s-home' },
+      { value: 'star', name: '重要', icon: 'el-icon-star-off' },
+      { value: 'task', name: '任务', icon: 'el-icon-s-home' },
     ]
     const menuNumMap = reactive({
       star: 10,
@@ -39,7 +66,23 @@ export default {
     })
     const activeMenu = ref('star')
 
-    return { userInfo, staticMenu, menuNumMap, activeMenu }
+    const toActive = function (item: any) {
+      activeMenu.value = item.value
+    }
+
+    const taskMenuList = reactive([
+      { value: 1, name: 'vadbds', tasks: [1, 2] },
+      { value: 2, name: 'vad版本发布bds', tasks: [1, 2] },
+    ])
+
+    return {
+      userInfo,
+      staticMenu,
+      menuNumMap,
+      activeMenu,
+      toActive,
+      taskMenuList,
+    }
   },
 }
 </script>
@@ -61,7 +104,7 @@ export default {
       margin-right: 20px;
     }
   }
-  .main-menu-item {
+  .menu-item {
     display: flex;
     align-items: center;
     padding: 0 20px;
@@ -71,8 +114,23 @@ export default {
     &:hover {
       background-color: rgba(175, 175, 175, 0.6);
     }
+    &.is-active {
+      font-weight: bold;
+      .active-line {
+        opacity: 1;
+      }
+    }
+    .active-line {
+      width: 3px;
+      height: 30px;
+      background-color: violet;
+      margin-right: 0;
+      opacity: 0;
+      margin-right: 20px;
+    }
     i {
       margin-right: 12px;
+      font-size: 18px;
     }
     .count {
       position: absolute;
